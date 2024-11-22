e7076df5aa08fc4e70992ee21a4906fd3d81195a
Plowix <PawelSzczypula@outlook.com>
2024-11-22 11:37:54 +0100

    CSS, ładowanie

--- cmentarz-frontend/src/components/HomePage/GraveInformationPanel/GraveInfo.jsx
+++ cmentarz-frontend/src/components/HomePage/GraveInformationPanel/GraveInfo.jsx
@@ -2,13 +2,16 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCross } from '@fortawesome/free-solid-svg-icons';
 
+import Spinner from "./Spinner";
 import PersonInfo from "./PersonInfo";
 function GraveInfo({selectedGraveId}){
     const [graveData, setGraveData] = useState({});
+    const [isLoading, setIsLoading] = useState(true)
 
     const apiUrl = process.env.REACT_APP_API_URL
     
     useEffect(() => {
+        setIsLoading(true);
         fetch(apiUrl+'/?grave_id='+encodeURIComponent(selectedGraveId))
             .then((response) => {
                 if (!response.ok) {
@@ -17,18 +20,24 @@ function GraveInfo({selectedGraveId}){
                 return response.json();
             })
             .then((data) => setGraveData(data))
+            .then(() => setIsLoading(false))
             .catch((error) => console.error("Błąd:", error));
     }, [selectedGraveId]);
 
     return(
         <div className="grave-information-panel">
+            {isLoading ? <Spinner/> : <>
             <img src={process.env.REACT_APP_URL+graveData.photo_path} alt="Zdjęcie grobu"/>
+            <div>
             <h2 className="grave-people-header"><FontAwesomeIcon icon={faCross}/> Pochowani:</h2>
             <div className="people-container">
                 {graveData['people'] ? 
                 graveData['people'].map(function(data){return(<PersonInfo key={data.full_name} personData={data}/>)}) :
                 ""}
             </div>
+            </div>
+            </>
+            }
         </div>
     )
 }
